92ee348eb0b149a9bab9e551cea40869cc6ab548
jianzhang.yjz@bytedance.com <jianzhang.yjz@bytedance.com>
2022-03-25 18:22:19 +0800

    init rep

---
+++ study.cpp
@@ -0,0 +1,17 @@
+#include<iostream> //引入标准io头文件
+
+int main() {
+
+    int val1 = 1;
+    double val2 = 3.14;
+
+
+    //C++11引入auto类型说明符，让编译器推断表达式类型
+    auto val3 = val1 + val2;
+
+    //decltype返回操作数或表达式的类型
+    decltype(val2) val4 = 3.1415;
+    int &val5 = val1;
+    //变量加上()则得到的类型是引用类型，对于引用类型需要初始化
+    decltype((val1)) val6 = val5;
+}
